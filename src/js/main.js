107a8aaa2f759f470735c1ddaf9409f4d921e65d
jose gomez <joseesapel@gmail.com>
2025-06-18 19:44:04 -0500

    feat:se ha creado  la pp de IMC

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,24 @@
-let peso
-let altura 
+const peso = document.getElementById('peso')
+const altura = document.getElementById('altura')
+const result = document.getElementById('result')
+const calcular= document.getElementById('calcular')
 
-peso = prompt("ingresa peso")
-altura = prompt("ingresa altura")
+calcular.addEventListener('click',function(){
+    
 
+const imc = peso.value / (altura.value * altura.value);
+let categoria = '';
 
+if (imc < 18.5) {
+    categoria = 'Bajo peso';
+} else if (imc < 25) {
+    categoria = 'Peso normal';
+} else if (imc < 30) {
+    categoria = 'Sobrepeso';
+} else {
+    categoria = 'Obesidad';
+}
 
+ result.textContent="tu tienes un imc de "+ Math.round(imc) +" por lo tanto esta en: "+ categoria
+})
 
